8b3381fb0d5b501bdbe8536b7bdf007009015779
emmabastas <emma.bastas@protonmail.com>
2022-06-14 10:39:21 +0200

    Emacs, preview markdown with multimarkdown

--- emma/doom-emacs/default.nix
+++ emma/doom-emacs/default.nix
@@ -1,7 +1,14 @@
-{ callPackage }:
+{ callPackage, multimarkdown }:
 
 callPackage (builtins.fetchTarball {
   url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
 }) {
   doomPrivateDir = ./.;
+
+  # Used for previews in markdown mode
+  extraPackages = [ multimarkdown ];
+
+  extraConfig = ''
+    (setq markdown-command "${multimarkdown}/bin/multimarkdown")
+  '';
 }
