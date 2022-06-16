fd890847613a248e892ebcb500270e8840eb60d3
emmabastas <emma.bastas@protonmail.com>
2022-06-16 21:02:48 +0200

    spellcheck with hunspell

--- emma/doom-emacs/config.el
+++ emma/doom-emacs/config.el
@@ -1,1 +1,24 @@
 (setq display-line-numbers 'relative)
+
+(with-eval-after-load "ispell"
+  ;; Configure 'LANG', otherwise 'ispell.el' cannot find a 'default
+  ;; dictionary' even though multiple dictionaries will be configured
+  ;; in next line.
+  (setenv "LANG" "en_US.UTF-8")
+  (setq ispell-program-name "hunspell")
+
+  (setq ispell-dictionary "en_US,sv_SE")
+
+  ;; 'ispell-set-spellchecker-params' has to be called
+  ;; before 'ispell-hunspell-add-multi-dic' will work
+  (ispell-set-spellchecker-params)
+  (ispell-hunspell-add-multi-dic "en_US,sv_SE")
+
+  ;; For saving words to the personal dictionary, don't infer it from
+  ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
+  (setq ispell-personal-dictionary "~/.hunspell_personal_dictionary"))
+
+;; The personal dictionary file has to exist, otherwise hunspell will
+;; silently not use it.
+(unless (file-exists-p ispell-personal-dictionary)
+  (write-region "" nil ispell-personal-dictionary nil 0))
--- emma/doom-emacs/default.nix
+++ emma/doom-emacs/default.nix
@@ -1,14 +1,25 @@
-{ callPackage, multimarkdown }:
+{ callPackage
+, multimarkdown
+, hunspellWithDicts
+, hunspellDicts
+}:
+
+let
+  hunspell = hunspellWithDicts ( with hunspellDicts; [ sv-se  en-us ] );
+in
 
 callPackage (builtins.fetchTarball {
   url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
 }) {
   doomPrivateDir = ./.;
 
-  # Used for previews in markdown mode
-  extraPackages = [ multimarkdown ];
+  extraPackages = [
+    multimarkdown   # Previews in markdown mode
+    hunspell        # Used by the `spell` module
+  ];
 
   extraConfig = ''
     (setq exec-path (append exec-path '("${multimarkdown}/bin")))
+    (setq exec-path (append exec-path '("${hunspell}/bin")))
   '';
 }
